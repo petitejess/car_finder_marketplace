cad36882e5ef82745e3ab26294584c92499d5f1c
petitejess <jessica.gozali@outlook.com.au>
2021-07-13 08:57:54 +1000

    Add static image, install action storage for image upload field and show image in show page.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # devise by default redirect to root path after login, so need to make after_sign_in method to redirect it somewhere else
+  def after_sign_in_path_for(current_user)
+    if current_user.profile
+      new_car_path
+    else
+      new_profile_path || root_path
+    end
+  end
 end
---
+++ app/models/review.rb
@@ -0,0 +1,7 @@
+class Review < ApplicationRecord
+  # belongs_to :profile
+  
+  belongs_to :buyer, class_name: "Profile", optional: true
+  belongs_to :seller, class_name: "Profile", option: true
+
+end
