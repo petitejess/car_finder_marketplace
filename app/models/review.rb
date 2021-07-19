52663021687b0025bf813c34e25d8ba0c219a565
petitejess <jessica.gozali@outlook.com.au>
2021-07-19 21:24:15 +1000

    Fix typo in model review rb file

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,6 @@ class Review < ApplicationRecord
   # belongs_to :profile
   
   belongs_to :buyer, class_name: "Profile", optional: true
-  belongs_to :seller, class_name: "Profile", option: true
+  belongs_to :seller, class_name: "Profile", optional: true
 
 end
